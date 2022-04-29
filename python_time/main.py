2f38771824bce07a5c83784940c8df49551f1be3
azamjon08 <104623940+azamjon08@users.noreply.github.com>
2022-04-29 15:33:36 +0500

    Add files via upload

---
+++ python_time/main.py
@@ -0,0 +1,14 @@
+import time
+
+def poyga():
+    print("Musobaqaga tayyorlaning...")
+    time.sleep(2)  
+    print("3") 
+    time.sleep(2)
+    print("2")
+    time.sleep(2)
+    print("1")
+    time.sleep(2)
+    
+    print("Start")
+poyga()    
